80f623ffa0e00241b32b4c16d90553068dbe510a
1361944487@qq.com <1361944487@qq.com>
2018-02-27 14:40:20 +0800

    1

---
+++ server/config/index.js
@@ -0,0 +1,7 @@
+export default {
+  wechat: {
+    AppID: 'wx6493035f0ac0d9c0',
+    AppSecret: 'f063a2c65153ca0f5a9793855f3d271e',
+    token: 'chenchunchengceshishuju'
+  }
+}
--- server/index.js
+++ server/index.js
@@ -1,39 +1,49 @@
 import Koa from 'koa'
-import { Nuxt, Builder } from 'nuxt'
+import Nuxt from 'nuxt'
+import R from 'ramda'
+import { resolve } from 'path'
 
-async function start () {
-  const app = new Koa()
-  const host = process.env.HOST || '127.0.0.1'
-  const port = process.env.PORT || 3006
+let config = require('../nuxt.config.js')
+config.dev = !(process.env === 'production')
 
-  // Import and Set Nuxt.js options
-  let config = require('../nuxt.config.js')
-  config.dev = !(app.env === 'production')
+const r = path => resolve(__dirname, path)
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 3006
+const MIDDLEWARES = ['router']
 
-  // Instantiate nuxt.js
-  const nuxt = new Nuxt(config)
-
-  // Build in development
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
+class Server {
+  constructor() {
+    this.app = new Koa()
+    this.useMiddleWares(this.app)(MIDDLEWARES)
   }
+  useMiddleWares(app) {
+    return R.map(R.compose(
+      R.map(i => i(app)),
+      require,
+      i => `${r('./middlewares')}/${i}`
+    ))
+  }
+  async start() {
+    const nuxt = await new Nuxt(config)
+    if (config.dev) {
+      try {
+        await nuxt.build()
+      } catch (e) {
+        process.exit(1)
+        console.log(e)
+      }
+    }
 
-  app.use(async (ctx, next) => {
-    await next()
-    ctx.status = 200 // koa defaults to 404 when it sees that status is unset
-    return new Promise((resolve, reject) => {
-      ctx.res.on('close', resolve)
-      ctx.res.on('finish', resolve)
-      nuxt.render(ctx.req, ctx.res, promise => {
-        // nuxt.render passes a rejected promise into callback on error.
-        promise.then(resolve).catch(reject)
-      })
+    this.app.use(async(ctx, next) =>{
+      ctx.status = 200
+      await nuxt.render(ctx.req, ctx.res)
     })
-  })
 
-  app.listen(port, host)
-  console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
+    this.app.listen(port, host)
+    console.log('Server listening on ' + host + ':' + port)
+  }
 }
 
-start()
+const app = new Server()
+
+app.start()
